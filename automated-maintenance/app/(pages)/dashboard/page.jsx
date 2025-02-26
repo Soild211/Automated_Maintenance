b0e2da55c9ce6cdef416ea28a7a12b63f3fbd121
Egon15 <pradnesh.tilekar_comp22@pccoer.in>
2025-02-26 18:28:08 +0530

    Admin issues page

---
+++ automated-maintenance/app/(pages)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminPage from "@/components/AdminPage";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <AdminPage />
+    </div>
+  );
+};
+
+export default Dashboard;
