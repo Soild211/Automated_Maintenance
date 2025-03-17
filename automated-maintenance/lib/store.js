fea8cc355b22a5df49c8596a1d195ac2807fa2d9
Suyash-shinde <suyash.c.shinde@gmail.com>
2025-03-17 23:04:38 +0530

    fixed redux persist

--- automated-maintenance/lib/store.js
+++ automated-maintenance/lib/store.js
@@ -1,26 +1,9 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import authReducer from "./slices/authSlice";
 import issuesReducer from "./slices/issuesSlice";
 import ticketsReducer from "./slices/ticketSlice";
-
-const persistConfig = {
-    key: 'root',
-    storage,
-};
-
-const rootReducer = combineReducers({
-    auth: authReducer,
-    issues: issuesReducer,
-    tickets: ticketsReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 
 const persistConfig = {
     key: 'root',
